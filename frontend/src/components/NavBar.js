dab31b840947dfc6673d3d3e8228e56fec12fc63
lukewtom93 <lukewtom93@gmail.com>
2026-03-31 18:43:07 +0100

    added styles for the navbar and app. Google fonts added to index.html

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           Lose It!
