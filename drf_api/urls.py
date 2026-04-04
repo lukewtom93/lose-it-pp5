e7a435b888053606313725e5a698fd6f33c0b091
lukewtom93 <lukewtom93@gmail.com>
2026-04-04 14:45:56 +0100

    Setup staticfiles for deployment

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.veiws.generic import TemplateView
+from django.views.generic import TemplateView
 from .views import logout_route
 
 urlpatterns = [
