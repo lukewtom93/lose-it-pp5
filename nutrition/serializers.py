15095fbf97ba35e9f29ced3e878bcd84acd1865d
lukewtom93 <lukewtom93@gmail.com>
2026-04-18 17:27:34 +0100

    meal enrty and daily calorie goal serializers added

--- nutrition/serializers.py
+++ nutrition/serializers.py
@@ -22,3 +22,39 @@ class FoodSerializer(serializers.ModelSerializer):
         ]
 
 
+class MealEntrySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    food_name = serializers.ReadOnlyField(source='food.name')
+    serving_unit = serializers.ReadOnlyField(source='food.serving_unit')
+
+    class Meta:
+        model = MealEntry
+        fields = [
+            'id',
+            'owner',
+            'food',
+            'food_name',
+            'meal_type',
+            'quantity',
+            'serving_unit',
+            'consumed_at',
+            'created_at',
+            'updated_at',
+            'total_calories',
+            'total_protein',
+            'total_carbs',
+            'total_fat',
+        ]
+
+
+class DailyCalorieGoalSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = DailyCalorieGoal
+        fields = [
+            'id',
+            'owner',
+            'date',
+            'calorie_goal',
+        ]
