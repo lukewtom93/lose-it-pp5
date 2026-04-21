2313d37500fddd557b3942a20cf0428026367a73
lukewtom93 <lukewtom93@gmail.com>
2026-04-21 18:53:49 +0100

    changed settings for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -26,21 +26,21 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [(
-#         'rest_framework.authentication.SessionAuthentication'
-#         if 'DEV' in os.environ
-#         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-#     )]
-# }
 REST_FRAMEWORK = {
-    # changed default from rest_framework.authentication.SessionAuthentication
     'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-        # if 'DEV' in os.environ
-        # else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )],
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
 }
+# REST_FRAMEWORK = {
+#     # changed default from rest_framework.authentication.SessionAuthentication
+#     'DEFAULT_AUTHENTICATION_CLASSES': [(
+#         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+#         # if 'DEV' in os.environ
+#         # else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+#     )],
+# }
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -58,8 +58,8 @@ if 'DEV' not in os.environ:
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'Lax'  #'none'
-JWT_AUTH_SECURE = False  #True
+JWT_AUTH_SAMESITE = 'none'  #'lax'
+JWT_AUTH_SECURE = True  #false
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
