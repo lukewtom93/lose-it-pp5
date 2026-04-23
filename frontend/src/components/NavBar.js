abc3d2dcc3973799aef37634c32be209baa02ba8
lukewtom93 <lukewtom93@gmail.com>
2026-04-23 11:42:35 +0100

    updated Readme.md

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,8 @@
-
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 import {
   useCurrentUser,
   useSetCurrentUser,
@@ -18,32 +17,40 @@ const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
-  const history = useHistory()
- 
-  const {expanded, setExpanded, ref} = useClickOutsideToggle();
+  const history = useHistory();
 
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
-      removeTokenTimestamp()
-      history.push('/signin')
+      removeTokenTimestamp();
+      history.push("/signin");
     } catch (err) {
       console.log(err);
-    }console.log(document.cookie);
+    }
+    console.log(document.cookie);
   };
-
 
   const loggedInIcons = (
     <>
-      
+      <NavLink
+        exact
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/"
+      >
+        <i className="fa-solid fa-gauge"></i>Dashboard
+      </NavLink>
       <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
-      <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40}/>
-      
-    
+      <Avatar
+        src={currentUser?.profile_image}
+        text={currentUser?.username}
+        height={40}
+      />
     </>
   );
   const loggedOutIcons = (
@@ -66,25 +73,25 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-           <img src={logo} alt="logo" height="45" />
+            <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)}  aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink
-              exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/"
-            >
-              <i className="fa-solid fa-gauge"></i>Dashboard
-            </NavLink>
-
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
